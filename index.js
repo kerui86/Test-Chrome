52f68ace3c485492d087ee8361d2ee4fab4d2914
kerui86 <47255354+kerui86@users.noreply.github.com>
2019-10-24 22:00:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ bot.on("ready", () => {
 var prefix = config.prefix;
 
 bot.on("message", (message) => {
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "hola")) {
-    message.reply("ey baby!");  
+  if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+  if (args[0].toLowerCase() === prefix + "hola") {
+      let answers = ["ey baby! ", "Hola, espero que traigas un salchichón para mí :yum: ", "Hola! podrías ayudarme a elegir un objeto para un talismán! tú tienes 
+buen gusto, siempre estás bonita!", "Hola! has dormido bien? tienes los ojos tan hundidos que podría encajar huevos de Boltue", "Toma un té con vitaminas para aguantar el día ¿ok?", "Voy llegando tarde!! me quedé explorando en la misión", "Has visto mi kit de navegacion?"]
+    message.reply(answers[Math.floor(Math.random() * answers.length)]);  
   }
   if (message.content.startsWith("Chrome, y Karenn?")) {
     message.channel.send("p-porque preguntas por ella tan de pronto?");
+  }
+     if (message.content.startsWith("Chrome perrito")) {
+    message.reply("No soy un perro!! soy un Hombre lobo!! UN HOMBRE LOBO :rage: ");
   }
   if (message.content === prefix + "pre") {
     message.channel.send("<@&635788170599989268> Preparaos, atacamos en unos minutos");
@@ -26,3 +30,9 @@ bot.on("message", (message) => {
   }
 });
 bot.login(process.env.token);       
+
+
+ 
+     
+       
+     
